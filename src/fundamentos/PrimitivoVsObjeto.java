e8d01efd7b9dfb40faa388562217813f41ed1fbc
Andradinho <raphaelandrade2013@gmail.com>
2021-01-23 14:29:56 -0300

    Entendendo a diferença entre TiposPrimitivos e Objeto

--- src/fundamentos/PrimitivoVsObjeto.java
+++ src/fundamentos/PrimitivoVsObjeto.java
@@ -2,4 +2,13 @@ package fundamentos;
 
 public class PrimitivoVsObjeto {
 
+	public static void main(String[] args) {
+		
+		String s = new String("Testo");
+		s.toUpperCase();
+		
+		// Wrappers são a versão objeto dos tipos primitivod
+		int a = 123;
+		System.out.println(a);
+	}
 }
