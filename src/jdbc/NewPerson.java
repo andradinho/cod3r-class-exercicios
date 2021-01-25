d47e72c9ef2eab2973a64ac8367d327a34a2b93e
Andradinho <raphaelandrade2013@gmail.com>
2021-01-25 17:15:04 -0300

    Creating a class to Insert records using the scanner and the PrepareStatement.

---
+++ src/jdbc/NewPerson.java
@@ -0,0 +1,28 @@
+package jdbc;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.Scanner;
+
+public class NewPerson {
+
+	public static void main(String[] args) throws SQLException {
+		
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("Informe o nome: ");
+		String name = input.nextLine();
+		
+		Connection connection = ConnectionFactory.getConnection();
+		
+		String sql = "INSERT INTO people (name) VALUES (?)";
+		PreparedStatement stmt = connection.prepareStatement(sql);
+		stmt.setString(1, name);
+		
+		stmt.execute();
+		
+		System.out.println("Person added successfully.");
+		input.close();
+	}
+}
