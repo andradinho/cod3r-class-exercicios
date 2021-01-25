ed1296143de082377a636e44f6457558daeb7b11
Andradinho <raphaelandrade2013@gmail.com>
2021-01-25 18:47:57 -0300

    Database query challenge.

---
+++ src/jdbc/ConsultPeople2.java
@@ -0,0 +1,43 @@
+package jdbc;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class ConsultPeople2 {
+
+	public static void main(String[] args) throws SQLException {
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("Dado da busca: ");
+		String data = input.nextLine();
+		
+		Connection connection = ConnectionFactory.getConnection();
+		
+		String sql = "SELECT * FROM people WHERE name LIKE ?";
+		PreparedStatement stmt = connection.prepareStatement(sql);
+		stmt.setString(1, '%' + data + '%');
+		System.out.println(stmt);
+		ResultSet result = stmt.executeQuery();
+		
+		List<Person> people = new ArrayList<>();
+		
+		while(result.next()) {
+			int code = result.getInt("code");
+			String name = result.getString("name");
+			people.add(new Person(code, name));
+		}
+		
+		for(Person p: people) {
+			System.out.println(p.getCode() + " => " + p.getName());
+		}
+		
+		stmt.close();
+		connection.close();
+		input.close();
+	}
+}
